4eaef0ba13ff8308df08307554008bfe7ff06f58
Tamara Qassem <tamara.qassem@gmail.com>
2023-11-30 15:45:25 +0400

    Updating the cleaning results

--- cleaning_results.py
+++ cleaning_results.py
@@ -6,6 +6,10 @@ df = pd.read_excel('datasets/race_results.xlsx')
 df_overall = df[df['Country'] == 'ALL']
 df_detail = df[df['Country'] != 'ALL']
 
+# Modify df_overall DataFrame
+df_overall = df_overall.drop(['Country', 'PTS'], axis=1)
+df_overall = df_overall.rename(columns={'POS': 'Country', 'NO': 'Date'})
+
 # Save DataFrames to Excel files
 df_overall.to_excel('datasets/race_results_overall.xlsx', index=False)
 df_detail.to_excel('datasets/race_results_detail.xlsx', index=False)
