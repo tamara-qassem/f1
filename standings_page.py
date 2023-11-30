aab26e1a62449779a5697b1e658a2bb5c40dabeb
Tamara Qassem <tamara.qassem@gmail.com>
2023-11-30 13:50:39 +0400

    Updating the standings page to a simple layout to begin with

--- standings_page.py
+++ standings_page.py
@@ -1,50 +1,46 @@
 # app.py
 import streamlit as st
 import pandas as pd
-import os
 
 def load_data():
     # Load data from Excel file
     excel_path = "datasets/driver_world_standings.xlsx"  
     df = pd.read_excel(excel_path)
-    # Add base URL to "Image Filename" column
-    image_base_url = "https://github.com/tamara-qassem/f1/blob/main/"
-    df["Image Filename"] = image_base_url + df["Image Filename"]
-
-    # Add base URL to "Number Filename" column
-    number_base_url = "https://github.com/tamara-qassem/f1/blob/main/"
-    df["Number Filename"] = number_base_url + df["Number Filename"]
-    
     return df
 
 def main():
     # Title
-    st.title("Driver Standings and Images")
+    st.title("Driver Standings")
 
     # Load data from Excel
     df = load_data()
 
-    # Iterate through rows, displaying two items per row
     for index, row in df.iterrows():
-        # Create a container with a light blue background using st.markdown
-        st.markdown(
-            f"""
-            <div style="background-color: #e6f7ff; padding: 15px; border-radius: 10px; margin-bottom: 15px;">
-                <h2>{row['Championship Standing']}. {row['Driver']}</h2>
-                <div style="display: flex; justify-content: space-between; margin-top: 10px;">
-                    <div style="flex: 1;">
-                        <p>{row['Points']} PTS</p>
-                        <p>{row['Car']}</p>
-                    </div>
-                    <div style="flex: 1;">
-                        <img src="data:image/png;base64,{row['Image Filename']}"  width="150">
-                        <img src="data:image/png;base64,{row['Number Filename']}" width="70">
-                    </div>
-                </div>
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
+
+
+        col1, col2 = st.columns([4,1])
+
+        # Display text and images for the first row
+        col1.header(f"{row['Championship Standing']}. {row['Driver']}")
+        col1.markdown(" ")
+        col1.markdown(f"<p style='font-size: 22px;'>{row['Points']} PTS</p>", unsafe_allow_html=True)
+        col1.markdown(" ")
+        col1.markdown(" ")
+        col1.markdown(f"<p style='font-size: 22px;'>{row['Car']}</p>", unsafe_allow_html=True)
+
+        col2.markdown(" ")
+        col2.markdown(" ")
+        col2.markdown(" ")
+        col2.image(row['Image Filename'], width=130)
+        col2.image(row['Number Filename'], width=70)
+        col2.markdown(" ")
+        col2.markdown(" ")
+
+
+
+
+        
+
 
 if __name__ == "__main__":
     main()
